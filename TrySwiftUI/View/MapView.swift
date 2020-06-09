5164302e3df94c64e928b80f8ee9ad670819b050
Kafin Salim <kafinsalim@gmail.com>
2020-06-09 23:50:41 +0700

    update map lat long to bandung

--- TrySwiftUI/View/MapView.swift
+++ TrySwiftUI/View/MapView.swift
@@ -16,7 +16,7 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011286, longitude: -116.166868)
+            latitude: -6.945399, longitude: 107.593806)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
