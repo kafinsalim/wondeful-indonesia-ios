d70848142680a2284e2164767b533ee9f5a998a4
Kafin Salim <kafinsalim@gmail.com>
2020-06-09 23:51:40 +0700

    update profile aligning

--- TrySwiftUI/View/Profile.swift
+++ TrySwiftUI/View/Profile.swift
@@ -10,65 +10,63 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                MapView()
-                    .edgesIgnoringSafeArea(.top)
-                    .frame(height: 300)
-                Image("me").resizable().aspectRatio(contentMode: .fit)
-                    .frame(width: 250, height: 250)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                    .shadow(radius: 16)
-                    .offset(y: -130)
-                    .padding(.bottom, -130)
-                
-                Text("Kafin Salim")
-                    .font(.title)
-                    .padding(.vertical, 8)
-                Text("kafinsalim@gmail.com")
-                    .font(.caption)
-                    .foregroundColor(.gray)
-                    .padding(.vertical, 6)
-                Text("Software Engineer, Bandung")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .padding(.bottom, 42)
-                Text("find out more on:").font(.subheadline)
-                    .font(.subheadline).padding(.bottom, 24)
-                
-                HStack {
-                    Text("Linkedin")
-                        .foregroundColor(.blue)
-                        .padding(.horizontal, 32)
-                        .onTapGesture() {
-                            UIApplication.shared.open(URL(string: "https://www.linkedin.com/in/kafinsalim")!)
-                    }
-                    Spacer()
-                    Text("Github")
-                        .foregroundColor(.blue)
-                        .padding(.horizontal, 32)
-                        .onTapGesture() {
-                            UIApplication.shared.open(URL(string: "https://github.com/kafinsalim")!)
-                    }
-                }.padding(.horizontal, 48).padding(.bottom, 32)
-                
-                HStack {
-                    Text("Twitter").foregroundColor(.blue)
-                        .padding(.horizontal, 32)
-                        .onTapGesture() {
-                            UIApplication.shared.open(URL(string: "https://twitter.com/kafinsalim")!)
-                    }
-                    Spacer()
-                    Text("Blog").foregroundColor(.blue)
-                        .padding(.horizontal, 32)
-                        .onTapGesture() {
-                            UIApplication.shared.open(URL(string: "https://kafinsalim.xyz")!)
-                    }
-                }.padding(.horizontal, 48)
-                
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            Image("me").resizable().aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 250)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                .shadow(radius: 16)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            Text("Kafin Salim")
+                .font(.title)
+                .padding(.vertical, 8)
+            Text("kafinsalim@gmail.com")
+                .font(.caption)
+                .foregroundColor(.gray)
+                .padding(.vertical, 6)
+            Text("Software Engineer, Bandung")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .padding(.bottom, 42)
+            Text("find out more on:").font(.subheadline)
+                .font(.subheadline).padding(.bottom, 24)
+            
+            HStack {
+                Text("Linkedin")
+                    .foregroundColor(.blue)
+                    .padding(.horizontal, 32)
+                    .onTapGesture() {
+                        UIApplication.shared.open(URL(string: "https://www.linkedin.com/in/kafinsalim")!)
+                }
                 Spacer()
-            }
+                Text("Github")
+                    .foregroundColor(.blue)
+                    .padding(.horizontal, 32)
+                    .onTapGesture() {
+                        UIApplication.shared.open(URL(string: "https://github.com/kafinsalim")!)
+                }
+            }.padding(.horizontal, 48).padding(.bottom, 32)
+            
+            HStack {
+                Text("Twitter").foregroundColor(.blue)
+                    .padding(.horizontal, 32)
+                    .onTapGesture() {
+                        UIApplication.shared.open(URL(string: "https://twitter.com/kafinsalim")!)
+                }
+                Spacer()
+                Text("Blog").foregroundColor(.blue)
+                    .padding(.horizontal, 32)
+                    .onTapGesture() {
+                        UIApplication.shared.open(URL(string: "https://kafinsalim.xyz")!)
+                }
+            }.padding(.horizontal, 48)
+            
+            Spacer()
         }.navigationBarTitle(Text("About Developer"), displayMode: .inline)
     }
 }
